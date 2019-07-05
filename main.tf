0e919856756f71ebbe7a58ee3b63936b72218113
Owais M <equa.monde@gmail.com>
2019-07-05 10:12:48 -0500

    Fixed the bug with ingress rules not being applied for web_sg

--- main.tf
+++ main.tf
@@ -104,6 +104,8 @@ resource "aws_security_group" "web_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ## HTTP Port
@@ -112,6 +114,8 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
